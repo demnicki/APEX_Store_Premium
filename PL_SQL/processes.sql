166c704c9e7b0da1b06eb1eeb2df990659e794eb
Adam Demnicki <adamdemnicki@gmail.com>
2024-10-01 14:02:40 +0200

    Global proceses.

--- PL_SQL/processes.sql
+++ PL_SQL/processes.sql
@@ -1,5 +1,33 @@
 ﻿/*
 Processes.
+
+Application processes - after authentication.
+*/
+
+DECLARE
+	v_id_user         users.id_user%TYPE;
+	v_permission_type users.permission_type%TYPE;
+	v_name_user       users.name_user%TYPE;
+	v_eur             users.balance_available_eur%TYPE;
+BEGIN
+	SELECT id_user,
+		permission_type,
+		name_user,
+		balance_available_eur
+		INTO
+		v_id_user,
+		v_permission_type,
+		v_name_user,
+		v_eur
+	FROM users WHERE login_email = lower(apex_application.g_user);
+	:EUR := v_eur;
+	:ID_USER := v_id_user;
+	:NAME_USER := v_name_user;
+	:PERMISSION_TYPE := v_permission_type;
+END;
+
+/*
+Processes for page 3.
 */
 DECLARE
 	if_successful BOOLEAN;
--- Page_2_src/storage.js
+++ Page_2_src/storage.js
@@ -1,24 +1,10 @@
 var all_local_storage;
-var login_mail = 'adam@wp.pl';
-var id_user = '1001';
-var order_nr = '12345678';
-var content_message = 'Your message';
-var add_app_1 = false;
-var add_app_2 = false;
-var add_app_3 = false;
-var add_app_4 = false;
-var nr_pieces_cloud = 0;
-var nr_pieces_windows = 0;
-var nr_pieces_h_consult = 0;
-var nr_pieces_h_programming = 0;
-
-function true_or_false(volumen) {
-    if (volumen)
-    { return 'true' }
-    else
-    { return 'false' };
-};
 function overwrite_local_storage() {
-    all_local_storage = '{ "login_mail": "' + login_mail + '", "order_nr": "' + order_nr + '", "content_message": "' + content_message + '", "add_app_1": ' + true_or_false(add_app_1) + ', "add_app_2": ' + true_or_false(add_app_2) + ', "add_app_3": ' + true_or_false(add_app_3) + ', "add_app_4": ' + true_or_false(add_app_4) + ', "nr_pieces_cloud": ' + nr_pieces_cloud + ', "nr_pieces_windows": ' + nr_pieces_windows + ', "nr_pieces_h_consult": ' + nr_pieces_h_consult + ', "nr_pieces_h_programming": ' + nr_pieces_h_programming +' }';
-    localStorage.setItem('general', all_local_storage);
+    all_local_storage = JSON.parse(localStorage.getItem('general'));
+    all_local_storage.login_email = apex.env.APP_USER;
+    localStorage.setItem('general', JSON.stringify(all_local_storage));
+};
+
+function tester() {
+    localStorage.setItem('general', '{"color":10,"login_email":"adamd@wp.pl","id_user":"1001","order_nr":"12345678","content_message":"Your message","add_app_1":false,"add_app_2":false,"add_app_3":false,"add_app_4":false,"nr_pieces_cloud":0,"nr_pieces_windows":0,"nr_pieces_h_consult":0,"nr_pieces_h_programming":0}');
 };
