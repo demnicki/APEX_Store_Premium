426acf255c3b31bd40fc7a114dbef0010c50042e
Adam Demnicki <adamdemnicki@gmail.com>
2024-10-24 17:07:09 +0200

    New Table

--- PL_SQL/tables.sql
+++ PL_SQL/tables.sql
@@ -68,31 +68,58 @@ CREATE TABLE employees(
 	CONSTRAINT c_login_emp FOREIGN KEY (login_email) REFERENCES users(login_email)
 );
 
+CREATE TABLE product_type(
+	id          CHAR(15 CHAR) NOT NULL,
+	description VARCHAR(2000 CHAR),
+	CONSTRAINT c_id_product_type PRIMARY KEY (id)
+);
+
+CREATE TABLE products(
+	id           NUMBER(2) NOT NULL,
+	product_type CHAR(1 CHAR) NOT NULL,
+	price        NUMBER(6, 2) DEFAULT 0.01 NOT NULL,
+	image        BLOB,
+	description  CLOB,
+	CONSTRAINT c_id_product PRIMARY KEY (id),
+	CONSTRAINT c_type_product FOREIGN KEY (product_type) REFERENCES product_type(id)
+);
+
 CREATE TABLE order_records(
-	id_order                NUMBER(8) DEFAULT ON NULL seq_orders.NEXTVAL NOT NULL,
-	date_created            DATE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	login_email             VARCHAR(100 CHAR) NOT NULL,
-	id_emp                  CHAR(2 CHAR) NOT NULL,
-	add_app_1               NUMBER(1) DEFAULT 0 NOT NULL,
-	add_app_2               NUMBER(1) DEFAULT 0 NOT NULL,
-	add_app_3               NUMBER(1) DEFAULT 0 NOT NULL,
-	add_app_4               NUMBER(1) DEFAULT 0 NOT NULL,
-	nr_pieces_cloud         NUMBER(3) DEFAULT 0 NOT NULL,
-	nr_pieces_windows       NUMBER(3) DEFAULT 0 NOT NULL,
-	nr_pieces_h_consult     NUMBER(3) DEFAULT 0 NOT NULL,
-	nr_pieces_h_programming NUMBER(3) DEFAULT 0 NOT NULL,
+	id_order       NUMBER(8) DEFAULT ON NULL seq_orders.NEXTVAL NOT NULL,
+	date_created   DATE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	session_number CHAR(15 CHAR) NOT NULL,
+	login_email    VARCHAR(100 CHAR) NOT NULL,
+	id_emp         CHAR(2 CHAR) NOT NULL,
 	CONSTRAINT c_id_order PRIMARY KEY (id_order),
+	CONSTRAINT c_session_number UNIQUE (session_number),
 	CONSTRAINT c_login_order FOREIGN KEY (login_email) REFERENCES users(login_email),
 	CONSTRAINT c_id_emp_order FOREIGN KEY (id_emp) REFERENCES employees(id_emp)
 );
 
+CREATE TABLE shopping_cart(
+	id_order   NUMBER(8) NOT NULL,
+	id_product NUMBER(2) NOT NULL,
+	quantity   NUMBER(3) DEFAULT 1 NOT NULL,
+	CONSTRAINT c_id_order_cart FOREIGN KEY (id_order) REFERENCES order_records(id_order),
+	CONSTRAINT c_id_product_cart FOREIGN KEY (id_product) REFERENCES products(id)
+);
+
+CREATE TABLE message_status(
+	id          CHAR (1 CHAR) NOT NULL,
+	description VARCHAR(500 CHAR) NOT NULL,
+	CONSTRAINT c_message_status PRIMARY KEY (id)
+);
+
 CREATE TABLE messages(
-	id_message      NUMBER(4) DEFAULT ON NULL seq_messages.NEXTVAL NOT NULL,
-	date_created    DATE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	login_email     VARCHAR(100 CHAR) NOT NULL,
-	id_emp          CHAR(2 CHAR) NOT NULL,
-	content_message CLOB,
+	id_message             NUMBER(4) DEFAULT ON NULL seq_messages.NEXTVAL NOT NULL,
+	date_created           DATE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	login_email            VARCHAR(100 CHAR) NOT NULL,
+	id_emp                 CHAR(2 CHAR) NOT NULL,
+	message_status         CHAR(1 CHAR) NOT NULL,
+	content_message        CLOB,
+	content_translation_pl CLOB,
 	CONSTRAINT c_id_message PRIMARY KEY (id_message),
 	CONSTRAINT c_login_mess FOREIGN KEY (login_email) REFERENCES users(login_email),
-	CONSTRAINT c_id_emp_mess FOREIGN KEY (id_emp) REFERENCES employees(id_emp)
+	CONSTRAINT c_id_emp_mess FOREIGN KEY (id_emp) REFERENCES employees(id_emp),
+	CONSTRAINT c_type_mess FOREIGN KEY (message_status) REFERENCES message_status(id)
 );
