69957998f5a0bdbfe945f06c07f12c40dbaefd6d
Adam Demnicki <adamdemnicki@gmail.com>
2024-11-04 16:12:55 +0100

    jsPSD autotable

--- Page_2_src/js_pdf.js
+++ Page_2_src/js_pdf.js
@@ -1,13 +1,25 @@
-﻿function pdf() {
-    var doc = new jsPDF();
+﻿window.jsPDF = window.jspdf.jsPDF;
+
+var table_user_info = [
+    ['Your ID', '&ID_USER.'], ['Funds available in your account in Euro currency.', '€ &EUR.'], ['Your full name', '&NAME_USER.']
+];
+
+var array_shop_cart = [];
+
+function pdf() {
+    var doc = new jsPDF("p", "pt", "a4");
+    doc.setFontSize(17);
+    doc.text(220, 40, "Basic user data");
     doc.autoTable({
-        theme: 'plain',
-        headStyles: { fontSize: 20 },
-        bodyStyles: { fontSize: 20, fontStyle: 'italic' },
-
-        head: [['Position in the table.', 'Variable value']],
-        body: [['You ID', '&ID_USER.'], ['You are logged in as:', '&LOGIN_EMAIL.'], ['Your current account balance in euro:', '&EUR.']],
-
+        startY: 60,
+        body: table_user_info,
+        theme: 'grid',
     });
-    doc.save('Raport_your_profil.pdf');
+    doc.text(100, 150, "Order Details: The contents of your shopping cart.");
+    doc.autoTable({
+        startY: 180,
+        body: [['Nr', 'Name product', 'Quantity', 'Cost'], array_shop_cart, ['Total costs: ', '&.']],
+        theme: 'grid',
+    });
+    doc.save('your_raport.pdf');
 };
