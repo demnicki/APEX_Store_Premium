5e770dc943b9b9c2c6178e5182bccc761cdb30b6
Adam Demnicki <adamdemnicki@gmail.com>
2024-11-05 15:10:13 +0100

    Register 2

--- Host_intro/js/basic_page.js
+++ Host_intro/js/basic_page.js
@@ -1,13 +1,19 @@
-﻿var all_local_storage;
+﻿var v_login_mail = '';
 
-function url_fb() {
-    window.open('http://www.facebook.com/groups/1744045656082133', '_blank');
+var all_local_storage;
+
+function social_med(nr) {
+    if (nr == 1) {
+        window.open('http://www.facebook.com/groups/1744045656082133', '_blank');
+    } else if (nr == 2) {
+        window.open('http://www.youtube.com/@demnicki', '_blank');
+    } else if (nr == 3) {
+        window.open('http://www.linkedin.com/in/adam-demnicki-b205211a7/', '_blank');
+    } else if (nr == 4) {
+        window.open('http://www.github.com/demnicki', '_blank');
+    };
+
 };
-
-function url_yt() {
-    window.open('http://www.youtube.com/@demnicki', '_blank');
-};
-
 function message_box() {
     apex.message.alert('This service is not ready yet.');
 };
@@ -18,60 +24,4 @@ function shopping_cart() {
 
 function login() {
     apex.navigation.redirect("f?p=" + apex.env.APP_ID + ":9999:" + apex.env.APP_SESSION);
-};
-
-function overwrite_local_storage() {
-    all_local_storage = JSON.parse(localStorage.getItem('general'));
-    all_local_storage.login_email = apex.env.APP_USER;
-    localStorage.setItem('general', JSON.stringify(all_local_storage));
-};
-
-function pdf() {
-    var doc = new jsPDF();
-    doc.autoTable({
-        theme: 'plain',
-        headStyles: { fontSize: 20 },
-        bodyStyles: { fontSize: 20, fontStyle: 'italic' },
-
-        head: [['Position in the table.', 'Variable value']],
-        body: [['You ID', '&ID_USER.'], ['You are logged in as:', '&APP_USER.'], ['Your current account balance in euro:', '&EUR.']],
-
-    });
-
-    doc.autoTable({
-        theme: 'plain',
-        headStyles: { fontSize: 10 },
-        bodyStyles: { fontSize: 8, fontStyle: 'italic' },
-
-        head: [['ID', 'Name', 'Country']],
-        body: [['1', 'Simon', 'Swedenddd'], ['2', 'Karl', 'Norway']],
-
-    });
-    doc.save('Raport_your_profil.pdf');
-};
-
-function register() {
-    apex.server.process(
-        'Register',
-        {
-            x01: apex.item("GENDER").getValue(),
-            x02: apex.item("LANGUAGE").getValue(),
-            x03: apex.item("NR_TEL").getValue(),
-            x04: apex.item("FULL_NAME").getValue()
-        },
-        {
-            success: function (Data) {
-                var object = JSON.parse(Data);
-                if (object.if_successful) {
-                    apex.navigation.redirect("f?p=" + apex.env.APP_ID + ":12:" + apex.env.APP_SESSION);
-                } else {
-                    apex.message.alert('Fatal error. Please try again or contact support. Comm: ' + Data);
-                };
-            },
-            dataType: "text"
-        },
-        {
-            error: function () { apex.message.alert('Fatal error. Please try again or contact support.'); }
-        }
-    );
 };
---
+++ Page_3_src/register.sql
@@ -0,0 +1,15 @@
+﻿DECLARE
+	if_successful BOOLEAN;
+
+BEGIN
+	authentication.create_customer(
+		in_login_email    => :LOGIN_EMAIL,
+		in_gender_user    => apex_application.g_x01,
+		in_language_user  => apex_application.g_x02,
+		in_nr_tel         => apex_application.g_x03,
+		in_name_user      => apex_application.g_x04,
+		out_if_successful => if_successful);
+	apex_json.open_object;
+	apex_json.write('if_successful', if_successful);
+	apex_json.close_object;
+END;
