7df488e1f81ea6dba65a87cc0219e8f875970c54
Adam Demnicki <adamdemnicki@gmail.com>
2024-11-06 13:27:00 +0100

    New items.

--- PL_SQL/processes.sql
+++ PL_SQL/processes.sql
@@ -3,10 +3,25 @@ Processes.
 
 The process of generating a user's login status.
 */
-
+DECLARE
+	v_id_user         users.id_user%TYPE;
+	v_name_user       users.name_user%TYPE;
+	v_eur             users.balance_available_eur%TYPE;
 BEGIN
+	:SHOP_CART_STATUS := 'Value of your shopping cart: '||shop.cart_value(apex_custom_auth.get_session_id)||' EUR.';
 	IF authentication.is_exist_user(lower(:LOGIN_EMAIL)) THEN
         :CHECK_LOGIN := 'Logged in as: '||lower(:LOGIN_EMAIL)||'.';
+		SELECT id_user,
+			name_user,
+			balance_available_eur
+			INTO
+			v_id_user,
+			v_name_user,
+			v_eur
+		FROM users WHERE login_email = lower(:LOGIN_EMAIL);
+		:EUR := v_eur;
+		:ID_USER := v_id_user;
+		:NAME_USER := v_name_user;
 	ELSE
         :CHECK_LOGIN := 'You are not logged in. Log in / register now.';
     END IF;
@@ -19,38 +34,3 @@ The process of refreshing the value of the entire shopping cart.
 BEGIN
 	:SHOP_CART_STATUS := 'Value of your shopping cart: '||shop.cart_value(apex_custom_auth.get_session_id)||' EUR.';
 END;
-
-/*
-Application processes - Authorization scheme - checking if the user is registered.
-*/
-BEGIN
-	IF authentication.is_exist_user(lower(:LOGIN_EMAIL)) THEN
-		RETURN true;
-	ELSE
-		apex_util.redirect_url (p_url => './register');
-	END IF;
-END;
-
-/*
-Processes for page 3.
-*/
-DECLARE
-	if_successful BOOLEAN;
-
-BEGIN
-	authentication.create_customer(
-		in_login_email    => :LOGIN_EMAIL,
-		in_gender_user    => apex_application.g_x01,
-		in_language_user  => apex_application.g_x02,
-		in_nr_tel         => apex_application.g_x03,
-		in_name_user      => apex_application.g_x04,
-		out_if_successful => if_successful);
-	apex_json.open_object;
-	apex_json.write('if_successful', if_successful);
-	apex_json.close_object;
-END;
-
-/*
-SQL queries for managing the shopping cart.
-*/
-
