457b0631d869b8e18aa95244fc6462c240f6f4ae
Adam Demnicki <adamdemnicki@gmail.com>
2025-02-18 20:34:29 +0100

    Completion of the project.

--- Page_14_src/cart.sql
+++ Page_14_src/cart.sql
@@ -4,3 +4,11 @@ BEGIN
     :P14_SUM_CART := shop.cart_value(apex_custom_auth.get_session_id);
     :P14_SUM_SUBS := shop.monthly_subs(apex_custom_auth.get_session_id);
 END;
+/*
+*/
+SELECT id_prod, name_product, quantity, cost, minus_product, add_product FROM quant_products;
+/*
+*/
+SELECT id_prod, name_product, cost, delete_product FROM subs_products;
+/*
+*/
